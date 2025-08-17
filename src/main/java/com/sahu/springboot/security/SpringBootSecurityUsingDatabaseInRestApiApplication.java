516cb7094d7fc9dbbba8015fb19d5630b87f22df
Nirmala Kumar Sahu <nirmalakumarsahu7@gmail.com>
2025-08-17 14:03:31 +0530

    intiatl Project Setup

---
+++ src/main/java/com/sahu/springboot/security/SpringBootSecurityUsingDatabaseInRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.sahu.springboot.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityUsingDatabaseInRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityUsingDatabaseInRestApiApplication.class, args);
+	}
+
+}
