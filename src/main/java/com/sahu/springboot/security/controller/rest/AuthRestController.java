012375ee1178fe093824a57ad85d4f126c8cca41
Nirmala Kumar Sahu <nirmalakumarsahu7@gmail.com>
2025-08-19 00:12:58 +0530

    dto naming removed

--- src/main/java/com/sahu/springboot/security/controller/rest/AuthRestController.java
+++ src/main/java/com/sahu/springboot/security/controller/rest/AuthRestController.java
@@ -1,8 +1,6 @@
 package com.sahu.springboot.security.controller.rest;
 
-import com.sahu.springboot.security.constant.AuthConstants;
 import com.sahu.springboot.security.dto.*;
-import com.sahu.springboot.security.model.Role;
 import com.sahu.springboot.security.model.User;
 import com.sahu.springboot.security.security.dto.CustomUserDetails;
 import com.sahu.springboot.security.security.util.SecurityUtil;
@@ -33,16 +31,16 @@ public class AuthRestController {
     private final AuthenticationManager authenticationManager;
 
     @PostMapping("/login")
-    public ResponseEntity<ApiResponse<LoginResponseDTO>> login(@RequestBody LoginRequestDTO loginRequestDTO, HttpServletRequest httpServletRequest) {
+    public ResponseEntity<ApiResponse<LoginResponse>> login(@RequestBody LoginRequest loginRequest, HttpServletRequest httpServletRequest) {
         Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(loginRequestDTO.username(), loginRequestDTO.password()));
+                new UsernamePasswordAuthenticationToken(loginRequest.username(), loginRequest.password()));
 
         if (authentication.isAuthenticated()) {
             SecurityContextHolder.getContext().setAuthentication(authentication);
 
             CustomUserDetails userDetails = SecurityUtil.getCurrentUser();
             return ResponseEntity.ok(ApiResponse.success(HttpStatus.OK, "User Login Successfully",
-                    LoginResponseDTO.builder()
+                    LoginResponse.builder()
                             .userId(userDetails.getUserId())
                             .username(userDetails.getUsername())
                             .email(userDetails.getEmail())
@@ -57,27 +55,27 @@ public class AuthRestController {
     }
 
     @PostMapping("/register")
-    public ResponseEntity<ApiResponse<?>> register(@RequestBody UserRequestDTO userRequestDTO, HttpServletRequest request) {
-        log.debug("Registration process started for user: {}", userRequestDTO.username());
+    public ResponseEntity<ApiResponse<?>> register(@RequestBody UserRequest userRequest, HttpServletRequest request) {
+        log.debug("Registration process started for user: {}", userRequest.username());
 
         //Check if the user already exists
-        if (userService.existsByUsername(userRequestDTO.username())) {
+        if (userService.existsByUsername(userRequest.username())) {
             return ResponseEntity.badRequest().body(ApiResponse.failure(HttpStatus.CONFLICT, "Username already exists",
                     null,
                     request.getRequestURI()));
         }
 
-        if (userService.existsByEmail(userRequestDTO.email())) {
+        if (userService.existsByEmail(userRequest.email())) {
             return ResponseEntity.badRequest().body(ApiResponse.failure(HttpStatus.CONFLICT, "Email already exists",
                     null,
                     request.getRequestURI()));
         }
 
         //Add the user
-        User user = userService.addUser(userRequestDTO);
+        User user = userService.addUser(userRequest);
         if (Objects.nonNull(user)) {
             return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.success(HttpStatus.CREATED, "Registration successful!",
-                    UserResponseDTO.builder()
+                    UserResponse.builder()
                             .userId(user.getId())
                             .username(user.getUsername())
                             .email(user.getEmail())
